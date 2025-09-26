5ac4e69639c623821ca9b676d001a779805d6f4d
Nathaniel Nathaniel <nathaniel_sigit@yahoo.com>
2025-09-27 01:31:39 +0200

    update

--- config.py
+++ config.py
@@ -1,10 +1,10 @@
 import os
 
-DS_ROOT = r"D:\Datasets\fcn_datasets\hand"
+DS_ROOT = r"D:\Datasets\fcn_datasets\face_object"
 RES_DIR = "result"
 LOG_DIR = "result/logs"
 
-N_EPOCHS = 1
+N_EPOCHS = 1000
 PATIENCE = 15
 LR = 0.001
 WEIGHT_DECAY = 1e-4
