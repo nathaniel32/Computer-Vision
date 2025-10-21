2c3dd08ed27efe5f049fe7cc9b6e194d957511c3
Nathaniel Nathaniel <nathaniel_sigit@yahoo.com>
2025-10-21 03:41:15 +0200

    update

--- helper/augment.py
+++ helper/augment.py
@@ -9,7 +9,7 @@ class ColorPartAugmenter: #tidak merusak norm
     def __init__(self, target_label=config.TARGET_CLASS_ID, p_aug=0.7):
         self.target_label = target_label
         self.p_aug = p_aug
-        self.color_augment_list = self.get_augment_list(hex_colors_list=["#FFFFFF", "#E4D5D5", "#C9AEAE", "#ADA5A5"])
+        self.color_augment_list = self.get_augment_list(hex_colors_list=["#FFFFFF", "#EBD5D5", "#B8B3B3", "#AA9D9D"])
     
     def _get_mask(self, labels):
         return labels == self.target_label
