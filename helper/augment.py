e4d763f1e2b43a659051bcf9800533379ef6a07d
Nathaniel Nathaniel <nathaniel_sigit@yahoo.com>
2025-10-23 03:10:56 +0200

    update

--- helper/augment.py
+++ helper/augment.py
@@ -221,7 +221,7 @@ class ObjectAugmenter: # merusak norm
                 #('jitter', {'sigma': 0.01}),
                 
                 # Color augmentations (recommended to enable)
-                ('brightness', {'brightness_range': (0.7, 1.3)}),
+                #('brightness', {'brightness_range': (0.7, 1.3)}),
                 #('contrast', {'contrast_range': (0.8, 1.2)}),
                 #('saturation', {'saturation_range': (0.7, 1.3)}),
                 #('hue_shift', {'hue_range': (-0.05, 0.05)}),
