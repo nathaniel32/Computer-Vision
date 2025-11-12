23cb85689755229dff17205314ec1eb96b6a6928
Nathaniel Nathaniel <nathaniel_sigit@yahoo.com>
2025-11-12 02:40:25 +0100

    update

---
+++ download_model.bat
@@ -0,0 +1,12 @@
+@echo off
+set URL=https://storage.googleapis.com/drive-bulk-export-anonymous/20251112T011258.505Z/4133399871716478688/fd025c1c-5adb-4625-b744-156a86d7fd76/1/07f4fb01-c58d-4a97-a734-e4a6b3dd22ec
+set ZIP=segmentation_model_tmp.zip
+set DEST=.
+
+if not exist "%DEST%" (
+    mkdir "%DEST%"
+)
+
+curl -L -o "%ZIP%" "%URL%"
+powershell -Command "Expand-Archive '%ZIP%' '%DEST%' -Force"
+del "%ZIP%"
