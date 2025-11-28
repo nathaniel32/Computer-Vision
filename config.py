7b6f89c9ad3b449a02c0d81a4cdfd69278199029
Nathaniel Nathaniel <nathaniel_sigit@yahoo.com>
2025-11-28 15:50:38 +0100

    update

--- config.py
+++ config.py
@@ -1,6 +1,5 @@
 import os
 
-DS_ROOT = r"D:\Datasets\3d\medico_part_color\datasets\latest"
 LOG_DIR = os.path.join(os.path.dirname(os.path.abspath(__file__)), "result")
 RES_DIR = os.path.join(os.path.dirname(os.path.abspath(__file__)), "result")
 
@@ -11,6 +10,7 @@ BATCH_SIZE = 12
 LR = 1e-3
 PATIENCE = 20
 
+DS_ROOT = r"D:\Datasets\3d\medico_part_color\datasets\latest"
 TARGET_COLOR_AUGMENT_LABELS = [0, 1]
 CLASSES = [
     {"label": "background", "color": "#FFFFFF"},
