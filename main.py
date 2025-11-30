42cc1a444817d321b1807c0be1ba31b2f46785a0
Nathaniel Nathaniel <nathaniel_sigit@yahoo.com>
2025-11-30 23:08:50 +0100

    update

--- main.py
+++ main.py
@@ -67,13 +67,15 @@ class Main:
             elif choice == "3":
                 input_dir_path = input("Input Dir Path: ").strip('"').strip()
                 output_dir_path = input("Output Dir Path: ").strip('"').strip()
+                plot_dir_path = input("Plot Dir Path: ").strip('"').strip() or None
                 keep_label = int(input("Keep Label ID: "))
-                Predict(self.config).cleaning_object(input_dir_path, output_dir_path, keep_label)
+                Predict(self.config).cleaning_object(input_dir_path, output_dir_path, keep_label, plot_dir=plot_dir_path)
             elif choice == "4":
                 input_dir_path = input("Input Dir Path: ").strip('"').strip()
                 output_dir_path = input("Output Dir Path: ").strip('"').strip()
+                plot_dir_path = input("Plot Dir Path: ").strip('"').strip() or None
                 real_marker_diameter_cm = float(input("Marker Diameter (cm): "))
-                Predict(self.config).scaling_object(input_dir_path, output_dir_path, real_marker_diameter_cm)
+                Predict(self.config).scaling_object(input_dir_path, output_dir_path, real_marker_diameter_cm, plot_dir=plot_dir_path)
             elif choice == "5":
                 print("""Expected folder structure:
                 input_dir/
