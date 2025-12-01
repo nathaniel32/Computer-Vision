bec59f9f0213b59e6415aa0fdeb5ce147b8116fb
Nathaniel Nathaniel <nathaniel_sigit@yahoo.com>
2025-12-01 02:29:53 +0100

    update

--- handler/predict.py
+++ handler/predict.py
@@ -67,7 +67,7 @@ class Predict:
         points, color, pred_label, mesh_file_path = self._predicting(input_dir_path, output_dir_path)
 
         if plot:
-            plot_point_cloud(points, color, self.classes, pred_label=pred_label, plot_tool=PlotTool.OPEN3D, save_dir=plot_dir, headless=headless)
+            plot_point_cloud(points, color, self.classes, pred_label=pred_label, plot_tool=PlotTool.OPEN3D, save_dir=plot_dir, headless=headless, file_base_name="cleaning")
         
         # trim mesh
         trim_out_path = os.path.join(output_dir_path, "trim_mesh.obj")
@@ -78,14 +78,14 @@ class Predict:
         remove_indecies = pred_label != keep_label # remove
         
         if plot:
-            plot_point_cloud(points[keep_indecies], color[keep_indecies], self.classes, pred_label=pred_label[keep_indecies], plot_tool=PlotTool.OPEN3D, save_dir=plot_dir, file_base_name="keep", headless=headless)
-            plot_point_cloud(points[remove_indecies], color[remove_indecies], self.classes, pred_label=pred_label[remove_indecies], plot_tool=PlotTool.OPEN3D, save_dir=plot_dir, file_base_name="remove", headless=headless)
+            plot_point_cloud(points[keep_indecies], color[keep_indecies], self.classes, pred_label=pred_label[keep_indecies], plot_tool=PlotTool.OPEN3D, save_dir=plot_dir, file_base_name="cleaning_keep", headless=headless)
+            plot_point_cloud(points[remove_indecies], color[remove_indecies], self.classes, pred_label=pred_label[remove_indecies], plot_tool=PlotTool.OPEN3D, save_dir=plot_dir, file_base_name="cleaning_remove", headless=headless)
 
     def scaling_object(self, input_dir_path, output_dir_path, real_marker_diameter_cm, plot=True, plot_dir=None, headless=False):
         points, color, pred_label, mesh_file_path = self._predicting(input_dir_path, output_dir_path)
 
         if plot:
-            plot_point_cloud(points, color, self.classes, pred_label=pred_label, plot_tool=PlotTool.OPEN3D, save_dir=plot_dir, headless=headless)
+            plot_point_cloud(points, color, self.classes, pred_label=pred_label, plot_tool=PlotTool.OPEN3D, save_dir=plot_dir, headless=headless, file_base_name="scaling")
 
         all_markers_metrics = []
         for label in self.config.scale_labels:
@@ -99,8 +99,8 @@ class Predict:
                 if plot:
                     #plot_marker_all_axes(points, center, results)
                     label_name = self.config.classes[label]['label']
-                    plot_marker_all_axes(marker_points, center, marker_axes_metrics, file_base_name=f"{label_name}_full", save_dir=plot_dir, headless=headless)
-                    plot_marker_all_axes(filtered_marker_points, center, marker_axes_metrics, file_base_name=f"{label_name}_filtered", save_dir=plot_dir, headless=headless)
+                    plot_marker_all_axes(marker_points, center, marker_axes_metrics, file_base_name=f"scaling_{label_name}_full", save_dir=plot_dir, headless=headless)
+                    #plot_marker_all_axes(filtered_marker_points, center, marker_axes_metrics, file_base_name=f"scaling_{label_name}_filtered", save_dir=plot_dir, headless=headless)
             except Exception as e:
                 print(e)
 
